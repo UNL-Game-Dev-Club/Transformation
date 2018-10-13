0470a595cef1b5ec1eda3f26d149ef61e0afea38
Robert Lafferty <rclafferty@ksu.edu>
2018-10-13 13:37:11 -0500

    Merged My Changes
    I Did a thing.
    
    I made a script to kill the zombies, which destroys it after 5 hits.

---
+++ Assets/Scripts/Zombies/EnemyHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    const int MAX_HEALTH = 5;
+    int health;
+
+	// Use this for initialization
+	void Start () {
+        health = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeHit()
+    {
+        health--;
+        if (health == 0)
+        {
+            DropLoot();
+
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void DropLoot()
+    {
+        // TODO: Drop random loot
+    }
+}
