393e9d8383d2af9753ce4be23c5dce27f9857491
yang112196 <yang112196@gmail.com>
2018-10-13 10:19:00 -0500

    HealthUI, zombie
    not completed zombie

---
+++ Assets/Scripts/Title/AIRunningController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class AIRunningController : MonoBehaviour {
+
+    bool isRunning;
+
+	// Use this for initialization
+	void Start () {
+        isRunning = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isRunning)
+        {
+            Debug.Log("Starting running");
+            isRunning = true;
+            GetComponent<ThirdPersonCharacter>().Move(new Vector3(13, 0, 0), false, false);
+
+        }
+	}
+}
