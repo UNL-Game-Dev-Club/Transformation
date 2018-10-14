dc3eb0a5875ae4cd2fb39cf54852a5bca5deca81
Robert Lafferty <rclafferty@ksu.edu>
2018-10-13 19:10:33 -0500

    Added Title Screen Functionality and Intro Sceen (everything)

--- Assets/Scripts/PlayerFightController.cs
+++ Assets/Scripts/PlayerFightController.cs
@@ -17,8 +17,20 @@ public class PlayerFightController : MonoBehaviour {
 	void Update () {
 		
 	}
-    
-    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Name: " + collision.collider.name + ", Tag: " + collision.collider.tag);
+        if (enemies.Contains(collision.collider.tag))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Attacking Zombie");
+                Attack(collision.collider.gameObject);
+            }
+        }
+    }
+
     private void OnCollisionStay(Collision collision)
     {
         Debug.Log("Name: " + collision.collider.name + ", Tag: " + collision.collider.tag);
---
+++ Assets/Scripts/Title/IntroText.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroText : MonoBehaviour {
+
+    Text introText;
+    Text whereAmI;
+    //float delay = 0.1f;
+    int sleepDelay = 75;
+    string text;
+    string text2;
+    string text3;
+    bool typing;
+    bool typing2;
+
+    int textIndex;
+
+    // Use this for initialization
+    void Start () {
+        introText = GameObject.Find("IntroText").GetComponent<Text>();
+        whereAmI = GameObject.Find("WhereAmI").GetComponent<Text>();
+        introText.text = "";
+        whereAmI.text = "";
+        text = "It was the year 2068.\n\nWorld War III was upon us, so I signed up as a volunteer for the U.S. Army.\n\nLittle did I know what was to become of me when I did...";
+        text2 = "WHERE AM I???";
+        text3 = "Congratulations, soldier. You have been selected to be a part of the special weapons project. You belong to the government now, so please try your best to be cooperative. Any attempts to escape will be futile.";
+        typing = true;
+        typing2 = true;
+        textIndex = 0;
+
+        Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (typing)
+        {
+            introText.text += text[textIndex++];
+            if (textIndex == text.Length)
+            {
+                typing = false;
+                System.Threading.Thread.Sleep(1500);
+                textIndex = 0;
+            }
+
+            System.Threading.Thread.Sleep(sleepDelay);
+        }
+        else if (typing2)
+        {
+            whereAmI.text += text2[textIndex++];
+            System.Threading.Thread.Sleep(sleepDelay * 3);
+            if (textIndex == text2.Length)
+            {
+                typing2 = false;
+            }
+        }
+        else
+        {
+            System.Threading.Thread.Sleep(3000);
+            SceneManager.LoadScene("stage1");
+        }
+	}
+
+    void Initialize()
+    {
+    }
+}
